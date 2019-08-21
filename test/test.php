7e8ec541bd8357e16bfc788afc482ef48901d885
Tier5 LLC <work@tier5.us>
2019-08-22 00:40:36 +0530

    Add Python3 suppot & fix Python path for test

--- test/test.php
+++ test/test.php
@@ -1,12 +1,12 @@
 <?php
 
-/* 
+/**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-include 'MyCypher.php';
+include __DIR__ . '/../MyCypher.php';
 
 $iv = 'c09f6a9e157d253d0b2f0bcd81d338298950f246';
 $cypher = new MyCypher($iv);
@@ -18,8 +18,9 @@ $secret = 'TestString From PHP';
 echo "================Encrypt Test ===================";
 echo "\nString = ".$secret;
 
+
 $php_encrypted      = $cypher->encrypt($secret);
-$python_encrypted   = exec('python ./test.py encrypt "'.$secret.'" '.$iv);
+$python_encrypted   = exec('python ' . __DIR__ . '/test.py encrypt "'.$secret.'" '.$iv);
 
 if($python_encrypted == $php_encrypted) {
     echo "\nTest - Success";
@@ -33,7 +34,7 @@ echo "\n================Encrypt PHP, Decrypt Python Test ===================";
 echo "\nString = ".$secret;
 
 $php_encrypted      = $cypher->encrypt($secret);
-$python_secret   = exec('python ./test.py decrypt "'.$php_encrypted.'" '.$iv);
+$python_secret   = exec('python ' . __DIR__ . '/test.py decrypt "'.$php_encrypted.'" '.$iv);
 
 if($secret == $python_secret) {
     echo "\nTest - Success";
@@ -48,7 +49,7 @@ echo "\n================Encrypt Python, Decrypt PHP Test ===================";
 echo "\nString = ".$secret;
 
 
-$python_secret   = exec('python ./test.py encrypt "'.$secret.'" '.$iv);
+$python_secret   = exec('python ' . __DIR__ . '/test.py encrypt "'.$secret.'" '.$iv);
 $php_encrypted      = $cypher->decrypt($python_secret);
 
 if($secret == $php_encrypted) {
